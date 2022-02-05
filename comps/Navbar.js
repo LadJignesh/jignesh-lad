f1036dd11ee762b04c190bce350fc408fa2603d7
“LadJignesh” <jigneshlad@Jigneshs-MacBook-Air.local>
2022-02-05 15:16:17 +0000

    addition of few pages and component

---
+++ comps/Navbar.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+    return ( 
+        <nav>
+            <div className="logo">
+                <h3>Jignesh Lad</h3>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;

1b2ef9225a468a7d0eb078e8047d0fb00fc0d20c
“LadJignesh” <jigneshlad@Jigneshs-MacBook-Air.local>
2022-02-05 15:18:11 +0000

    changes to title in head

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Ninja List | Home</title>
+        <title>Jignesh Lad | Home</title>
         <meta name='keywords' content='ninjas'></meta>
       </Head>
       <div>
